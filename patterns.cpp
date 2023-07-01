c65875e68d1f294043bb4d1e0dbeb3dafb506273
Shivam Kaushal <shivamkaushal907@gmail.com>
2023-07-01 22:19:54 +0530

    solved some pattern questions

---
+++ patterns.cpp
@@ -0,0 +1,280 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // for(int i=0;i<5;i++){
+    //     for(int j=0;j<5;j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int i=0;i<5;i++){
+    //     for(int j=0;j<=i;j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int i=0;i<5;i++){
+    //     for(int j=0;j<=i;j++){
+    //         cout<<j+1<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int i=0;i<5;i++){
+    //     for(int j=0;j<=i;j++){
+    //         cout<<i+1<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int i=0;i<5;i++){
+    //     for(int j=5;j>i;j--){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int i=5;i>0;i--){
+    //     for(int j=0;j<i;j++){
+    //         cout<<j+1<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    //    for(int i=0;i<5;i++){
+    //     for(int j=5;j>i;j--){
+    //         cout<<" ";
+    //     }
+    //     for(int j=0;j<2*i+1;j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    //    for(int i=5;i>0;i--){
+    //     for(int j=5;j>=i;j--){
+    //         cout<<" ";
+    //     }
+    //     for(int j=2*i-1;j>0;j--){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+//Pattern 9
+
+    //        for(int i=0;i<5;i++){
+    //     for(int j=5;j>i;j--){
+    //         cout<<" ";
+    //     }
+    //     for(int j=0;j<2*i+1;j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    //    for(int i=5;i>0;i--){
+    //     for(int j=5;j>=i;j--){
+    //         cout<<" ";
+    //     }
+    //     for(int j=2*i-1;j>0;j--){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    // return 0;
+
+
+    //Pattern 10
+    // for(int i=0;i<5;i++){
+    //     for(int j=0;j<i;j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int i=0;i<5;i++){
+
+    //     for(int k=4;k>=i;k--){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    //pattern 11
+    //     for(int i=0;i<5;i++){
+    //     for(int j=0;j<=i;j++){
+    //         cout<<(i+j+1)%2<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    //pattern 12
+    //     for(int i=0;i<4;i++){
+    //     for(int j=0;j<=i;j++){
+    //         cout<<j+1;
+    //     }
+    //     for(int k=3;k>i;k--){
+    //         cout<<" ";
+    //     }
+       
+    //     for(int k=3;k>i;k--){
+    //         cout<<" ";
+    //     }
+    //     for(int j=0;j<=i;j++){
+    //         cout<<i+1-j;
+    //     }
+       
+    //     cout<<endl;
+    // }
+
+    //pattern 13
+//    int c=1;
+//    for(int i=0;i<5;i++){
+//     for(int j=0;j<=i;j++){
+//         cout<<c++<<" ";
+//     }
+//     cout<<endl;
+//    }
+
+//   //pattern 14
+//   char c='A';
+//   for (char i='A';i<='E';i++){
+//     for(char j='A'; j<=i;j++){
+//         cout<<j<<" ";
+
+//     }
+//     cout<<endl;
+//   }
+//   //pattern 15
+//   for (char i='E';i>='A';i--){
+//     for(char j='A'; j<=i;j++){
+//         cout<<j<<" ";
+
+//     }
+//     cout<<endl;
+//   }
+  //pattern 16
+//     for (char i='A';i<='E';i++){
+//     for(char j='A'; j<=i;j++){
+//         cout<<i<<" ";
+
+//     }
+//     cout<<endl;
+//   }
+
+    //pattern 17
+    //        for(char i='A';i<'E';i++){
+    //     for(char j='E';j>i;j--){
+    //         cout<<" ";
+    //     }
+    //     for(char j='A';j<=i;j++){
+    //         cout<<j;
+    //     }
+    //     for(char j='A';j<i;j++){
+    //         cout<<j;
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    //pattern 18
+    // for(char i='E';i>='A';i--){
+    //     for(char j=i;j<='E';j++){
+    //         cout<<j<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    //pattern 19
+    // for(int i=0;i<5;i++){
+    //     for(int j=5;j>i;j--){
+    //         cout<<"*";
+    //     }
+    //     for(int k=0;k<i;k++){
+    //         cout<<" ";
+    //     }
+    //     for(int k=0;k<i;k++){
+    //         cout<<" ";
+    //     }
+    //     for(int j=5;j>i;j--){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int i=0;i<5;i++){
+    //     for(int j=0;j<=i;j++){
+    //         cout<<"*";
+    //     }
+    //     for(int k=4;k>i;k--){
+    //         cout<<" ";
+    //     }
+    //     for(int k=4;k>i;k--){
+    //         cout<<" ";
+    //     }
+    //     for(int j=0;j<=i;j++){
+    //         cout<<"*";
+    //     }
+   
+    //     cout<<endl;
+    // }
+
+    //pattern 20
+    //     for(int i=0;i<5;i++){
+    //     for(int j=0;j<=i;j++){
+    //         cout<<"*";
+    //     }
+    //     for(int k=4;k>i;k--){
+    //         cout<<" ";
+    //     }
+    //     for(int k=4;k>i;k--){
+    //         cout<<" ";
+    //     }
+    //     for(int j=0;j<=i;j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int i=0;i<4;i++){
+    //     for(int j=4;j>i;j--){
+    //         cout<<"*";
+    //     }
+    //     for(int k=0;k<=i;k++){
+    //         cout<<" ";
+    //     }
+    //     for(int k=0;k<=i;k++){
+    //         cout<<" ";
+    //     }
+    //     for(int j=4;j>i;j--){
+    //         cout<<"*";
+    //     }
+   
+    //     cout<<endl;
+    // }
+
+    //pattern 21
+    // for(int i=0;i<4;i++){
+    //     for(int j=0;j<4;j++){
+    //         if(i==0 || j==0 || i==3 || j==3){
+    //             cout<<"* ";
+    //         }
+    //         else{
+    //             cout<<"  ";
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+
+    //pattern 22
+    for(int i=7;i>0;i--){
+        for(int j=7;j>0;j--){
+            if(i==1 || j==1 || i==7 || j==7){
+                cout<<"4 ";
+            }
+            else if(i==2 || j==2 || i==6 || j==6){
+                cout<<"3 ";
+            }
+            else if(i==3 || j==3 || i==5 || j==5){
+                cout<<"2 ";
+            }
+            else{
+                cout<<"1 ";
+            }
+            
+        }
+        cout<<endl;
+    }
+}
